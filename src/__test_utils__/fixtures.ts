fe566c84ac9967001479218777073d707f198e57
Sem G <sem.gebresilassie@fira.fi>
2022-05-19 17:56:14 +0300

    chore: test fixtures

---
+++ src/__test_utils__/fixtures.ts
@@ -0,0 +1,36 @@
+import { Account } from '../types/Account'
+
+export const mockTestAccounts: Account[] = [
+  {
+    id: 1,
+    ownerName: 'Jaakko',
+    lastName: 'S',
+    address: '777 Main St.',
+    city: 'Espoo',
+    balance: 0,
+  },
+  {
+    id: 2,
+    ownerName: 'Teemu',
+    lastName: 'P',
+    address: '1100 Blvd.',
+    city: 'Addis Ababa',
+    balance: 0,
+  },
+  {
+    id: 3,
+    ownerName: 'Anna',
+    lastName: 'Ro',
+    address: '45 Ita',
+    city: 'Helsinki',
+    balance: 0,
+  },
+  {
+    id: 4,
+    ownerName: 'Sem',
+    lastName: 'G',
+    address: '96 Viikki',
+    city: 'Ababa',
+    balance: 0,
+  },
+]
